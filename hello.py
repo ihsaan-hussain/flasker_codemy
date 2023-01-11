ef59babfb00fb3f0039c04443dc9d82f7efb4e83
Ihsaan Hussain <ihsaan09@outlook.com>
2023-01-11 18:18:52 +0000

    part 8

--- hello.py
+++ hello.py
@@ -2,18 +2,41 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Create A Flask Instance
 app = Flask(__name__)
+# Add Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# Secret Key!
 app.config['SECRET_KEY'] = "1234"
+# Initialize the Database
+db = SQLAlchemy(app)
+
+# Create Model
+class Users(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(200), nullable=False)
+	email = db.Column(db.String(120), nullable=False, unique=True)
+	date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+	# Create A String
+	def __repr__(self):
+		return '<Name %r>' % self.name 
+
+app.app_context().push()
+
+# Create a Form class
+class UserForm(FlaskForm):
+	name = StringField("Name", validators=[DataRequired()])
+	email = StringField("Email", validators=[DataRequired()])
+	submit = SubmitField("Submit")
 
 # Create a Form class
 class NamerForm(FlaskForm):
 	name = StringField("What's Your Name", validators=[DataRequired()])
 	submit = SubmitField("Submit")
-
-# Create a route decorator
-@app.route('/')
 
 #def index():
 #	return "<h1>Hello World!</h1>"
@@ -27,7 +50,28 @@ class NamerForm(FlaskForm):
 #trim
 #striptags
 
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+	name = None
+	form = UserForm()
+	if form.validate_on_submit():
+		user = Users.query.filter_by(email=form.email.data).first()
+		if user is None:
+			user = Users(name=form.name.data, email=form.email.data)
+			db.session.add(user)
+			db.session.commit()
+		name = form.name.data
+		form.name.data = ''
+		form.email.data = ''
+		flash("User Added Successfully!")
+	our_users = Users.query.order_by(Users.date_added)
+	return render_template("add_user.html",
+		form=form,
+		name=name,
+		our_users=our_users)
 
+# Create a route decorator
+@app.route('/')
 def index():
 	first_name = "Ihsaan"
 	stuff = "This is bold text"
