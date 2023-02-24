cf164a66ae1238ccc2d08637db818276cf3f014a
Ihsaan Hussain <ihsaan09@outlook.com>
2023-02-24 18:55:16 +0000

    part 27

--- hello.py
+++ hello.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
-from wtforms.validators import DataRequired, EqualTo, Length
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import date
-from wtforms.widgets import TextArea
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user 
+from webforms import LoginForm, PostForm, PasswordForm, UserForm, PasswordForm, NamerForm
 
 # Create A Flask Instance
 app = Flask(__name__)
@@ -30,11 +27,6 @@ login_manager.login_view = 'login'
 @login_manager.user_loader
 def load_user(user_id):
 	return Users.query.get(int(user_id))
-
-class LoginForm(FlaskForm):
-	username = StringField("Username", validators=[DataRequired()])
-	password = PasswordField("Password", validators=[DataRequired()])
-	submit = SubmitField("Submit")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -63,24 +55,31 @@ def logout():
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
+	form = UserForm()
+	id = current_user.id
+	name_to_update = Users.query.get_or_404(id)
+	if request.method == "POST":
+		name_to_update.name = request.form['name']
+		name_to_update.email = request.form['email']
+		name_to_update.favourite_color = request.form['favourite_color']
+		name_to_update.username = request.form['username']
+		try:
+			db.session.commit()
+			flash("User Updated Successfully!")
+			return render_template("dashboard.html",
+				form=form,
+				name_to_update=name_to_update)
+		except:
+			flash("Error! Looks like there was a problem")
+			return render_template("dashboard.html",
+				form=form,
+				name_to_update=name_to_update)
+	else:
+		return render_template("dashboard.html",
+			form=form,
+			name_to_update=name_to_update,
+			id=id)
 	return render_template('dashboard.html')
-
-# #Create a blog post model
-class Posts(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(255))
-	content = db.Column(db.Text)
-	author = db.Column(db.String(255))
-	date_poseted = db.Column(db.DateTime, default=datetime.utcnow)
-	slug = db.Column(db.String(255))
-
-# Create a posts form
-class PostForm(FlaskForm):
-	title = StringField("Title", validators=[DataRequired()])
-	content = StringField("Content", validators=[DataRequired()], widget=TextArea()) 
-	author = StringField("Author", validators=[DataRequired()])
-	slug = StringField("Slug", validators=[DataRequired()])
-	submit = SubmitField("Submit")
 
 
 @app.route('/posts/delete/<int:id>')
@@ -117,6 +116,7 @@ def post(id):
 	return render_template('post.html', post=post)
 
 @app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
 def edit_post(id):
 	post = Posts.query.get_or_404(id)
 	form = PostForm()
@@ -138,6 +138,7 @@ def edit_post(id):
 
 # Add Post Page
 @app.route('/add-post', methods=['GET', 'POST'])
+#@login_required
 def add_post():
 	form = PostForm()
 
@@ -170,33 +171,6 @@ def get_current_date():
 	return favourite_pizza
 	#return {"Date": date.today()}
 
-# Create Model
-class Users(db.Model, UserMixin):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(20), nullable=False, unique=True)
-	name = db.Column(db.String(200), nullable=False)
-	email = db.Column(db.String(120), nullable=False, unique=True)
-	favourite_color = db.Column(db.String(120))
-	date_added = db.Column(db.DateTime, default=datetime.utcnow)
-	# Do Some Password Stuff
-	password_hash = db.Column(db.String(128))
-
-
-	@property
-	def password(self):
-		raise AttributeError('password is not a readable attribute')
-
-	@password.setter
-	def password(self, password):
-		self.password_hash = generate_password_hash(password)
-
-	def verify_password(self, password):
-		return check_password_hash(self.password_hash, password)
-
-	# Create A String
-	def __repr__(self):
-		return '<Name %r>' % self.name
-
 @app.route('/delete/<int:id>')
 def delete(id):
 	user_to_delete = Users.query.get_or_404(id)
@@ -219,19 +193,10 @@ def delete(id):
 
 app.app_context().push()
 
-# Create a Form class
-class UserForm(FlaskForm):
-	name = StringField("Name", validators=[DataRequired()])
-	username = StringField("Username", validators=[DataRequired()])
-	email = StringField("Email", validators=[DataRequired()])
-	favourite_color = StringField("Favourite Color")
-	password_hash = PasswordField("Password", validators=[DataRequired(), EqualTo('password_hash2', message='Passwords Must Match!')])
-	password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
-	submit = SubmitField("Submit")
-
 
 # Update Databse Record
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
+@login_required
 def update(id):
 	form = UserForm()
 	name_to_update = Users.query.get_or_404(id)
@@ -239,6 +204,7 @@ def update(id):
 		name_to_update.name = request.form['name']
 		name_to_update.email = request.form['email']
 		name_to_update.favourite_color = request.form['favourite_color']
+		name_to_update.username = request.form['username']
 		try:
 			db.session.commit()
 			flash("User Updated Successfully!")
@@ -255,16 +221,6 @@ def update(id):
 			form=form,
 			name_to_update=name_to_update,
 			id=id)
-
-class PasswordForm(FlaskForm):
-	email = StringField("What's Your Email", validators=[DataRequired()])
-	password_hash = PasswordField("What's Your Password", validators=[DataRequired()])
-	submit = SubmitField("Submit")
-
-# Create a Form class
-class NamerForm(FlaskForm):
-	name = StringField("What's Your Name", validators=[DataRequired()])
-	submit = SubmitField("Submit")
 
 #def index():
 #	return "<h1>Hello World!</h1>"
@@ -380,3 +336,38 @@ def name():
 		name = name, 
 		form = form)
 
+# #Create a blog post model
+class Posts(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(255))
+	content = db.Column(db.Text)
+	author = db.Column(db.String(255))
+	date_poseted = db.Column(db.DateTime, default=datetime.utcnow)
+	slug = db.Column(db.String(255))
+
+# Create Model
+class Users(db.Model, UserMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), nullable=False, unique=True)
+	name = db.Column(db.String(200), nullable=False)
+	email = db.Column(db.String(120), nullable=False, unique=True)
+	favourite_color = db.Column(db.String(120))
+	date_added = db.Column(db.DateTime, default=datetime.utcnow)
+	# Do Some Password Stuff
+	password_hash = db.Column(db.String(128))
+
+
+	@property
+	def password(self):
+		raise AttributeError('password is not a readable attribute')
+
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
+	# Create A String
+	def __repr__(self):
+		return '<Name %r>' % self.name
---
+++ webforms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms.widgets import TextArea
+
+
+class LoginForm(FlaskForm):
+	username = StringField("Username", validators=[DataRequired()])
+	password = PasswordField("Password", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+class PostForm(FlaskForm):
+	title = StringField("Title", validators=[DataRequired()])
+	content = StringField("Content", validators=[DataRequired()], widget=TextArea()) 
+	author = StringField("Author", validators=[DataRequired()])
+	slug = StringField("Slug", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+class UserForm(FlaskForm):
+	name = StringField("Name", validators=[DataRequired()])
+	username = StringField("Username", validators=[DataRequired()])
+	email = StringField("Email", validators=[DataRequired()])
+	favourite_color = StringField("Favourite Color")
+	password_hash = PasswordField("Password", validators=[DataRequired(), EqualTo('password_hash2', message='Passwords Must Match!')])
+	password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+class PasswordForm(FlaskForm):
+	email = StringField("What's Your Email", validators=[DataRequired()])
+	password_hash = PasswordField("What's Your Password", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+class NamerForm(FlaskForm):
+	name = StringField("What's Your Name", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
